9d91f1a40f9d74299319c52d4e66e6fd937faa30
janWICKEDpro <janroyal45@gmail.com>
2024-10-10 21:56:50 +0100

    app colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //will change to our own when we have better colors
+        primary: "#FFFFFF", // White
+        secondary: "#673BB7", // Purple Color
+        accent: "#1A73E8", //  blue color
+        neutral: "#F8F9FA", // Custom neutral gray
+        background: "#F0EBF8", // Custom background color
+        text: "#202124", // Custom text color
+        textLight: "#5F6368", // light grey text color
+      },
+    },
   },
   plugins: [],
 };
