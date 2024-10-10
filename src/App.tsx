0b43b3f907b8ef27a4026ad852b946aa6b8054a9
janWICKEDpro <janroyal45@gmail.com>
2024-10-10 21:39:49 +0100

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App App-header">
       <h1> Dynamic Formz </h1>
+      <h2 className="text-1xl font-bold underline"> Tailwind</h2>
     </div>
   );
 }
